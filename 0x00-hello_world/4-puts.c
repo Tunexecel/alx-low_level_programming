5765259dd6374647428836978730c97261ddd66c
Tunexecel <tunexecel@gmail.com>
2022-10-13 14:22:37 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 /**
  *main - Entry point
@@ -10,16 +9,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-=======
-#include<stdio.h>                                                                                                           
-  2 /**                                                                                                                         
-  3  * main - Entry point                                                                                                       
-  4  *                                                                                                                          
-  5  * Return: Always 0 (Success)                                                                                               
-  6  */                                                                                                                         
-  7 int main(void)                                                                                                              
-  8 {                                                                                                                           
-  9 puts("\"Programming is like building a multilingual puzzle");                                                               
- 10 return (0);                                                                                                                 
- 11 }
->>>>>>> b494ac353f604e2ed293ee81e575659fcf505ca8
