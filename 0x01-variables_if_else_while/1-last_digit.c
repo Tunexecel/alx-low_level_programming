d26a7b0b1911ad2ef139e520f93bbb3e217fc03a
Tunexecel <tunexecel@gmail.com>
2022-10-14 15:37:41 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main-program entry point.
+ * main-program entry point.
  *
- *Return:0 if no error, non-zero value if error.
+ * Return:0 if no error, non-zero value if error.
  */
 int main(void)
 {
