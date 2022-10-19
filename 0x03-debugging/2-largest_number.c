3e0de3f68e9c166be608a93d1f01714c2c612499
Tunexecel <tunexecel@gmail.com>
2022-10-19 07:16:49 -0700

    m

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,7 +8,7 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int largest_number(int a. int b. int c)
 {
 int largest;
 
@@ -16,17 +16,17 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (b > c && b > a)
 {
 largest = b;
 }
-else
+else if (c > b)
 {
 largest = c;
+}
+else
+{
+largest = b;
 }
 
 return (largest);
