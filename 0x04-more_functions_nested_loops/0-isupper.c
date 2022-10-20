d8f2076a6642bd4a10b5809c383d0fea23bb11c2
Tunexecel <113934436+Tunexecel@users.noreply.github.com>
2022-10-20 23:38:42 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
-*_isupper - checks if a  character is uppercase or not
-*@c: character to be tested
-*Return: 1 whether it is, 0 otherwise
-*/
-
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
-	{
 		return (1);
-	}
-
-	return (0);
+	else
+		return (0);
 }
