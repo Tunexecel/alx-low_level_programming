1b900377e7208e9fd93165e767be306be870faf7
Tunexecel <113934436+Tunexecel@users.noreply.github.com>
2022-10-21 00:14:13 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-*mul - multiplies two numbers
-*@a: first number
-*@b: second number
-*Return: returns result
-*/
-
+ * mul - a function that multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the 2 inputs
+ */
 int mul(int a, int b)
 {
 	return (a * b);
