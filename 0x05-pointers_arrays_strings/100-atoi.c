8923e4c3769dba62b1ec04621e72433fca9a0dc4
Tunexecel <tunexecel@gmail.com>
2022-10-24 11:05:51 -0700

    m

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 
 /**
  * _atoi - convert a string to an integer.
- * @s: the string to be comverted.
+ * @s:the string to be comverted.
  *
  *Return: The integer value of the comverted string.
  */
@@ -12,7 +12,7 @@ int _atoi(char *s)
 int sign = 1;
 unsigned int num = 0;
 
-do {
+do{
 if (*s == '-')
 sign *= -1;
 
@@ -21,8 +21,7 @@ num = (num * 10) + (*s - '0');
 
 else if (num > 0)
 break;
-} 
-while (*s++);
+}while (*s++);
 
 return (num * sign);
 }
