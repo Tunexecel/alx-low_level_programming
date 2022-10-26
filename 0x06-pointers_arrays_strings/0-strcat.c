c7dedbd55f45f85ebbf14774a2785fbf27b35657
Tunexecel <tunexecel@gmail.com>
2022-10-26 11:38:59 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,7 @@
  * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest.
  */
+
 char *_strcat(char *dest, const char *src)
 {
 int index = 0, dest_len = 0;
